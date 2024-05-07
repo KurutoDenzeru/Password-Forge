a53e3bf7191ce4dfe5b31911bdb7aa00439d605a
Kuruto Denzeru <kurtdenzel51@gmail.com>
2024-05-07 13:24:18 +0800

    refactor: adding inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import "./App.css";
-
 function App() {
 	return (
 		<>
-			<body className="h-screen flex flex-col justify-center items-center mx-auto">
+			<body className="flex justify-center items-center h-screen">
 				<div className="max-w-6xl p-6 bg-white border border-gray-200 rounded-lg shadow ">
 					<h5 className="mb-2 text-start text-2xl font-bold tracking-tight text-gray-900">
 						Password Generator
 					</h5>
+					{/* Inputs */}
+					<input
+						type="text"
+						id="password"
+						className="bg-gray-50 mb-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:ring w-full p-2.5 "
+						readOnly
+					/>
 					<input
 						type="text"
 						id="password"
@@ -19,7 +24,7 @@ function App() {
 						{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
 						<button
 							id="copy-btn"
-							className="px-6 py-2 mb-4 bg-gray-500 w-full  
+							className="px-6 py-2 bg-gray-500 w-full  
                            text-white rounded-md 
                            hover:bg-gray-600 
                            focus:outline-none"
@@ -27,15 +32,17 @@ function App() {
 							Copy Password
 						</button>
 
-						{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-						<button
-							id="generate-btn"
-							className="px-6 py-2 bg-blue-500 text-white w-full
+						<div className="mb-4 flex flex-col">
+							{/* biome-ignore lint/a11y/useButtonType: <explanation> */}
+							<button
+								id="generate-btn"
+								className="px-6 py-2 mt-4 bg-blue-500 text-white w-full
                            rounded-md hover:bg-blue-600 
                            focus:outline-none"
-						>
-							Generate Password
-						</button>
+							>
+								Generate Password
+							</button>
+						</div>
 					</div>
 				</div>
 			</body>
